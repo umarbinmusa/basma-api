7ca0575b55419e65be8ae5a45d5762fdbda0eb81
unknown <umarmusa20002@gmail.com>
2021-06-23 17:14:51 +0100

    NEW

--- index.php
+++ index.php
@@ -80,7 +80,7 @@ function checklen()
                 <div class="row">
                     <div class="col-md-3 register-left" style="margin-top: 10%;right: 5%">
                         <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
-                        <h3>WELCOME </h3>
+                        <h3>BASMAH WELCOME <br/> YOU ALL</h3>
                        
                     </div>
                     <div class="col-md-9 register-right" style="margin-top: 40px;left: 80px;">
